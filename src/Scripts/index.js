a4d2445249d1f4ed3d6b56a1d3378430d9cf6fdf
Moll <herr.pedromoll@gmail.com>
2024-12-05 20:32:07 -0300

    Third commit, added a clock and fixed minor problems

--- src/Scripts/index.js
+++ src/Scripts/index.js
@@ -1,12 +1,13 @@
 const slides = document.querySelectorAll(".Home_slides section");
 let indexSlides = 0;
-let isTransitioning  //Para bloquear cliques muito rápidos
+let isTransitioning;  //Para bloquear cliques muito rápidos
 
-document.addEventListener("DOMContentLoaded", initialize)  //Quando a pagina carregar, chama a função "initialize"
-
+document.addEventListener("DOMContentLoaded", initialize);  //Quando a pagina carregar, chama a função "initialize"
+document.addEventListener("DOMContentLoaded", updateClock);  //Quando a pagina carregar, chama a função "initialize"
+setInterval(updateClock, 1000)
 
 function initialize() {
-    slides[indexSlides].classList.add("Display_slide")
+    slides[indexSlides].classList.add("Display_slide");
 }
 
 
@@ -46,3 +47,16 @@ function showSlide(index){
     });
     slides[index].classList.add("Display_slide");
 }
+
+function updateClock(){
+    const time = new Date();
+    const year = time.getFullYear().toString().padStart(4, "0");
+    const month = (time.getMonth() + 1).toString().padStart(2, "0");
+    const day = time.getDate().toString().padStart(2, "0");
+    const hour = time.getHours().toString().padStart(2, "0");
+    const minute = time.getMinutes().toString().padStart(2, "0");
+    const second = time.getSeconds().toString().padStart(2, "0");
+    const dateString = `${hour}:${minute}:${second} ${day}/${month}/${year}`;
+    document.getElementById("HomeDate").textContent = dateString;   
+}
+
